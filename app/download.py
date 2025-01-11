eb11bd2ec501d562dc2bcb597bc94216b31f92b0
Chabachib <nouhtahachebchoub@gmail.com>
2025-01-11 13:24:31 +0100

    Works good

--- app/download.py
+++ app/download.py
@@ -1,12 +1,15 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+from pathlib import Path
 
 def audio_download(url):
+    download_path = Path.home() / "Downloads"
     yt = YouTube(url, on_progress_callback=on_progress)
     ys = yt.streams.get_audio_only()
-    ys.download()
+    ys.download(output_path=download_path)
 
 def video_download(url):
+    download_path = Path.home() / "Downloads"
     yt = YouTube(url, on_progress_callback=on_progress)
     ys = yt.streams.get_highest_resolution()
-    ys.download()
+    ys.download(output_path=download_path)
